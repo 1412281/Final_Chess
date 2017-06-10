96b50fb14524e8f6328812e11681fce821cdc6f1
LAMTRAN\LAMTRAN <lamtran2601@gmail.com>
2017-06-10 13:37:32 +0700

    abstract board, chess model

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,5 +1,34 @@
 package model;
 
-public class Board {
+import java.util.List;
 
+public class Board {
+	private List<Chess> listBlackChess;
+	private List<Chess> listWhileChess;
+	
+	public boolean checkWin() {
+		return false;
+	}
+	
+	public boolean checkPosibleMove() {
+		return false; 
+	}
+	
+	
+	public List<Chess> getListBlackChess() {
+		return listBlackChess;
+	}
+	
+	
+	public void setListBlackChess(List<Chess> listBlackChess) {
+		this.listBlackChess = listBlackChess;
+	}
+	
+	public List<Chess> getListWhileChess() {
+		return listWhileChess;
+	}
+	
+	public void setListWhileChess(List<Chess> listWhileChess) {
+		this.listWhileChess = listWhileChess;
+	}
 }
--- src/main/java/model/Chess.java
+++ src/main/java/model/Chess.java
@@ -1,5 +1,44 @@
 package model;
+import java.util.List;
 
 public class Chess {
-
+	
+	private boolean team;
+	private int id;
+	private boolean live = true;
+	private Position pos;
+	
+	public List<Position> getPosibleMove() {
+		return null;
+	}
+	
+	public boolean getTeam() {
+		return team;
+	}
+	public void setTeam(boolean team) {
+		this.team = team;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public boolean isLive() {
+		return live;
+	}
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+	
+	public Position getPos() {
+		return pos;
+	}
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+	
 }
+
